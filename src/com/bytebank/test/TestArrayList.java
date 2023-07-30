a4c40301b29faa65de73eff005a6bda84f4766aa
Felix <felixnicolas-m@hotmail.com>
2023-07-30 11:26:28 -0300

    Proyecto bytebank - TestArrayList - ArrayList

--- src/com/bytebank/test/TestArrayList.java
+++ src/com/bytebank/test/TestArrayList.java
@@ -2,19 +2,33 @@ package com.bytebank.test;
 
 import com.bytebank.modelo.Cuenta;
 import com.bytebank.modelo.CuentaCorriente;
+import com.bytebank.modelo.cliente.Cliente;
 
 import java.util.ArrayList;
 
 public class TestArrayList {
     public static void main(String[] args) {
-        ArrayList lista = new ArrayList();
-        Cuenta cc = new CuentaCorriente(11,22);
-        Cuenta cc2 = new CuentaCorriente(22,44);
+        // <> Generic - Fuerza a que acepte solo un tipo de objeto
+        ArrayList<Cuenta> lista = new ArrayList<>();
+        Cuenta cc = new CuentaCorriente(11, 22);
+        Cuenta cc2 = new CuentaCorriente(22, 44);
 
         lista.add(cc);
         lista.add(cc2);
 
-        Cuenta obtenerCuenta = (Cuenta) lista.get(0);
+        Cliente cliente = new Cliente();
+        //lista.add(cliente);
+
+        Cuenta obtenerCuenta = lista.get(0);
         System.out.println(obtenerCuenta);
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+        }
+
+        for (Cuenta cuenta : lista){
+            System.out.println(cuenta);
+        }
+
     }
 }
